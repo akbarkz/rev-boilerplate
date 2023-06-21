0086f52a259e570eebe6012c3bcf0ce3be6763f9
Akbar Abdrakhmanov <akbar@Akbars-MacBook-Pro.local>
2023-06-21 22:28:58 +0600

    add changes to package.json after installing app via npx command

--- bin/index.js
+++ bin/index.js
@@ -15,7 +15,6 @@ const projectPath = path.join(currentPath, projectName);
 const git_repo = 'git@github.com:akbarik/rev-boilerplate.git';
 
 try {
-  console.log(projectPath);
   fs.mkdirSync(projectPath);
 } catch (err) {
   if (err.code === 'EEXIST') {
@@ -42,6 +41,24 @@ async function main() {
     execSync('rm -rf ./.git');
     execSync('rm README.md');
     fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true });
+
+    console.log(`Adding corresponding changes to package.json`);
+    const packageJsonPath = './package.json';
+
+    const packageJsonData = fs.readFileSync(packageJsonPath, 'utf-8');
+    const packageJson = JSON.parse(packageJsonData);
+
+    // Update the "name" field
+    packageJson.name = projectName;
+
+    // Update the "version" field
+    packageJson.version = '0.0.1';
+
+    // Remove the "bin" field
+    delete packageJson.bin;
+
+    // Write the updated JSON back to the file
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 
     console.log('The installation is done, you can proceed with developing your new app!');
   } catch (err) {
